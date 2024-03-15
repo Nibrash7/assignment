96a48b4348a74cfb6c3622a94875838ba8eadd99
User <nibrashmostakimworking@gmail.com>
2024-03-15 15:27:57 +0600

    first assignment

---
+++ bin/class1.dart
@@ -0,0 +1,19 @@
+class Media{
+
+  void play(){
+    print('Playing media...');
+  }
+}
+
+class Song extends Media {
+
+  String artist;
+  Song ({required this.artist});
+
+
+  @override
+  void play(){
+
+    print('Playing song by $artist');
+  }
+}
